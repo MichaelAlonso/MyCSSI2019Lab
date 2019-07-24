1948d2fc3e6c666a20345f5e47dec1807160cb02
Michael AlonsoAmbrocio <alons028@csusm.edu>
2019-07-24 12:00:23 -0700

    using console.

---
+++ WeLearn/M2-javascript/L1-JSIntro/Script.js
@@ -0,0 +1,11 @@
+console.log("script running");
+
+let name = "Charlie";
+console.log("Hello" + name + "!");
+let age = 15;
+console.log("You are" + age + "years old!");
+if (age >= 16) {
+ console.log("I can Drive!")
+} else {
+ console.log("I can't drive!")
+}
