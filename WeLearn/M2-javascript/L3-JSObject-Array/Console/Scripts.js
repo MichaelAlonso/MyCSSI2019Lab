f8bc9d664061c813ef10bd976f86d3049549cede
Michael AlonsoAmbrocio <alons028@csusm.edu>
2019-07-25 12:11:07 -0700

    numbers

---
+++ WeLearn/M2-javascript/L3-JSObject-Array/Console/Scripts.js
@@ -0,0 +1,27 @@
+const article = {
+  name: "Dog family gives birth to 10 puppies",
+  views: 1035,
+  datePublished: "03/25/2018",
+  author: {
+    name: "Joe Corgi",
+    title: "Senior Canine Editor"
+  }
+};
+const object = {
+  object: "glasses",
+};
+const movie = {
+  action: "avengers endgame",
+  romance: "none",
+  suspense: "What happened to Monday",
+  family: "Cheaper by the dozen",
+  Horror: "Us",
+};
+const seasons = ["Spring", "Summer", "Fall", "Winter"];
+const friends = ["Alexis", "Logan", "Melissa"];
+const numbers = [5, 10, 2, 16, 50];
+friends.join("~");
+
+const classes = [];
+classes.push ({course: "cs 111", title: "computer science I"});
+classes.push ({course: "math 160", title: "calculus I"});
